ab172ee9c8cd85dfb759269ed193caa924c1fe75
laurith <pene.com@gmail.com>
2026-07-31 14:07:04 -0500

    dependencia dotenv, 1 forma

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 import express from 'express';
-
+import {configDotenv} from "dotenv"
+configDotenv()
 
 const app = express();
-const port = 3000; 
+const port = process.env.PUERTO || 3000; 
 
 app.get("/", (_, res) => {
 res.send("Aprendicez ficha 3407186");
 });
 
 app.listen(port, () => {
-console.log( `Servidor en funcionamiento en el puerto: `);
+console.log( `Servidor: http://localhost:${port} `);
 });
