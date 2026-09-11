0b8577eb7dd140133042d6f4f6d0b03ff083fc8e
Nicol <260516927+nicoltriana71-source@users.noreply.github.com>
2026-09-11 14:14:32 -0500

    Creacion de la funcion de error

--- middlerware/manejadorErrores.js
+++ middlerware/manejadorErrores.js
@@ -1,5 +1,10 @@
 const manejadorErrores = (req, res, next) => {
+    const codigoEstado = error.statuCode || 500
+    const mensaje = error.mensaje || "Error inesperado."
+    console.error(`[ERROR] - ${new Date().toISOString()} - ${codigoEstado} - ${mensaje}`)
 
-    next()
+    //Validar si hay mas informacion 
+
+    
 }
 
